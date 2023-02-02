dcad68e64f466ad5e29dd99744fc221d13b6bb9f
BOURNET Pierre (SNCF VOYAGEURS / DDL GASCOGNE PYR LAURAGAIS / DIRECTION UO TRACTION GPL) <7906971j@commun.ad.sncf.fr>
2023-02-02 11:16:06 +0100

    merge 02/02/23

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,40 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+@IsString()
+@IsNotEmpty()
+public username: string;
+
+@IsEmail()
+public mail: string;
+
+@IsString()
+@IsNotEmpty()
+public password:  string
+
+@IsString()
+@IsNotEmpty()
+public adress_line1: string;
+
+@IsString()
+@IsNotEmpty()
+public adress_line2: string;
+
+@IsString()
+@IsNotEmpty()
+public adress_line3: string;
+
+@IsString()
+@IsNotEmpty()
+public zipCode: string;
+
+@IsString()
+@IsNotEmpty()
+public city: string;
+
+
+
+
+
+}
+
